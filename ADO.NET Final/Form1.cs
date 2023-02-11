244e0452f9a1adf1586ee1cf3652e61b26dd53db
Hemi_bk15 <Hemi_bk15@STHQ0115-04>
2023-02-11 09:42:09 +0400

    Configure

--- ADO.NET Final/Form1.cs
+++ ADO.NET Final/Form1.cs
@@ -1,10 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
 namespace ADO.NET_Final
 {
     public partial class Form1 : Form
     {
+        string connectionString = null;
         public Form1()
         {
             InitializeComponent();
+            Configure();
+        }
+
+
+        private void Configure()
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory() + "../../../../")
+                                  .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+
+            connectionString = configuration.GetConnectionString("SqlServerLibrary");
         }
     }
 }
